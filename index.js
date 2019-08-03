d8902ca3319b24b830668466d0d5f72120ec6ed5
Sylla Ismaila <syldox@gmail.com>
2019-08-03 14:20:34 +0400

    setting up google strategy options

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-app.get('/', (req, res)=>{
-    res.send({yo: 'this is a test'});
-});
+passport.use(new GoogleStrategy({
+    clientID: keys.GoogleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken) =>{
+    console.log(accessToken)
+}));
+
 
 const PORT = process.env.PORT || 5000;
 
